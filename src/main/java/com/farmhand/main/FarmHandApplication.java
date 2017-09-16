214ec85d6dda5e0ef0d4d8c8eab1e966963d8d7d
HelmutSteiner <helmut.steinersti2@gmail.com>
2017-09-16 11:09:22 +0200

    Initial commit.

---
+++ src/main/java/com/farmhand/main/FarmHandApplication.java
@@ -0,0 +1,24 @@
+package com.farmhand.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class FarmHandApplication {
+
+  @Bean
+  public EmbeddedServletContainerCustomizer containerCustomizer() {
+    return ((ConfigurableEmbeddedServletContainer containerCustomizer) ->  {
+
+
+    });
+  }
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmHandApplication.class, args);
+	}
+}
